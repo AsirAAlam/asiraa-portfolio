87d121b3cd7a0e785f0b67968f48db809483e031
AsirAAlam <Alvi.Abrar29@gmail.com>
2022-08-23 20:30:07 -0400

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
-import './App.css';
-import './Fonts.css';
-import { Box, Container, IconButton } from '@mui/material';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-
-const iconStyle = { height: 100, width: 100, cursor: 'pointer' };
+import HomePage from './HomePage';
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Container>
-        <h2 className="quicksand" style={{ fontWeight: 500, fontSize: 120 }}>
-          Asir A. Alam
-        </h2>
-        <h2 className='quicksand' style={{ fontWeight: 300, fontSize: 80, marginTop: -130 }}>
-          Personal Portfolio
-        </h2>
-      </Container>
-
-      <GitHubIcon sx={{...iconStyle}} onClick={() => { }} />
-      <LinkedInIcon sx={{...iconStyle}} onClick={() => { }} />
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum molestias quis repellat expedita veniam dolor fugit cupiditate, aut eius voluptates deleniti facere ducimus voluptatibus saepe earum nam porro exercitationem harum!
-        Expedita ducimus quibusdam fugit eius beatae nesciunt in dicta, deleniti sint, tempora nulla placeat aut commodi voluptatum porro delectus, ut quae iusto quaerat quasi amet adipisci praesentium pariatur dignissimos? Sit.
-      </p>
+    <div>
+      <Routes>
+        <Route path="/test" element={<h1>Test</h1>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </div>
   );
 }
