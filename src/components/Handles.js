4a4080b2daba7754b7cc41794324c0a1656fb32c
Asir Alam <alvi.abrar29@gmail.com>
2023-07-15 14:08:12 -0700

    Remove unused import in Handles.js

--- src/components/Handles.js
+++ src/components/Handles.js
@@ -1,4 +1,3 @@
-import { Container } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import SimCardDownloadIcon from '@mui/icons-material/SimCardDownload';
