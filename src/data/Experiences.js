7a15143de1f518506bcd7d86cc8cb9bf3e961e4d
Asir Alam <alvi.abrar29@gmail.com>
2023-08-30 17:07:23 -0400

    updated experiences

--- src/data/Experiences.js
+++ src/data/Experiences.js
@@ -2,16 +2,16 @@ const experiences = [
   {
     "title": "Meta",
     "role": "Software Engineering Intern",
-    "description": "Two internships over two summers. Worked with FB App Monetization and FB Gaming: Play + Fantasy in 2022 and 2023, respectively.",
-    "startDate": "Dec 2020",
-    "endDate": "Jun 2021"
+    "description": "Two internships over two summers. Worked with Facebook Shop Ads in 2022 and " +
+      "Facebook Gaming in 2023."
   },
   {
     "title": "UCF Programming Team",
     "role": "ICPC Competitive Programmer",
-    "description": "Competed in ICPC contests over my 3 years on the team so far. Solved hundreds of ICPC-style problems in preparation. Helped as a UCF High School Programming Tournament Judge by writing problems and helping develop problem sets.",
-    "startDate": "September 2020",
-    "endDate": "Present"
+    "description": "Competed in ICPC contests over my 3 years on the team. " +
+      "Solved hundreds of ICPC-style problems. " +
+      "Helped as a UCF High School Programming Tournament Judge by writing problems " +
+      "and helping develop problem sets."
   }
 ];
 
