69501cdab5ba1b5d64972d367208cee05053a6aa
Asir Alam <alvi.abrar29@gmail.com>
2025-01-12 18:33:56 -0500

    navbar button using router link

--- src/components/NavBarButton.js
+++ src/components/NavBarButton.js
@@ -1,12 +1,33 @@
 import React from "react";
+import { Link, useLocation } from "react-router";
+
 import "./NavBarButton.css";
 
 export default function NavBarButton({ label, href }) {
+  const location = useLocation();
+
+  let isCurrent = location.pathname === href;
+
   return (
-    <a href={href}>
-      <button className="mybutton bg-gradient-to-r from-sky-600 from-50% to-sky-950 to-50%">
+    <Link to={href}>
+      <button
+        className={
+          "mybutton bg-gradient-to-r from-sky-600 from-50% to-sky-950 to-50%"
+        }
+      >
         {label}
       </button>
-    </a>
+
+      <div
+        className="bg-sky-900"
+        style={{
+          height: "0.2em",
+          width: "100%",
+          borderRadius: 10,
+          display: isCurrent ? "block" : "none",
+          transition: "all 0.7s",
+        }}
+      />
+    </Link>
   );
 }
